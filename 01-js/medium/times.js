50b385b7972e4e81d204f32fb49a758fbb6c0356
Ashutosh <ashutoshshelke07@gmail.com>
2023-06-21 06:15:21 +0530

    added times function

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,12 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+    const start = Date.now()
+    sum = 0
+    for (let i = 0; i <= n; i++) {
+        sum += i
+    }
+    return (Date.now() - start) / 1000;
 }
+
+console.log(calculateTime(1000000))
